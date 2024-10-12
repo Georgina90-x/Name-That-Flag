7178d75eb7da40d5738d9842f08ebdf2a5598759
Georgina Kidger <georgina.kidger@hotmail.com>
2024-10-12 03:42:47 +0000

    ran js through jslint and made amendments

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 //Select Elements
-let countSpan = document.querySelector('.count span');
-let flagImgDiv = document.querySelector('.flag-img');
-let flagImg = document.querySelector('.flag-img img');
-let flagOptions = document.querySelector('.flag-options ul');
-let flagLis = document.querySelectorAll('.flag-options ul li');
-let score = document.querySelector('h3 span');
-let scoreDiv = document.querySelector('.score');
-let correctAns = document.querySelector('.score .right span');
-let incorrectAns = document.querySelector('.score .incorrect span');
-let btnNewGame = document.querySelector('#newGame');
-const timerElement = document.getElementById('time')
+let countSpan = document.querySelector(".count span");
+let flagImgDiv = document.querySelector(".flag-img");
+let flagImg = document.querySelector(".flag-img img");
+let flagOptions = document.querySelector(".flag-options ul");
+let flagLis = document.querySelectorAll(".flag-options ul li");
+let score = document.querySelector("h3 span");
+let scoreDiv = document.querySelector(".score");
+let correctAns = document.querySelector(".score .right span");
+let incorrectAns = document.querySelector(".score .incorrect span");
+let btnNewGame = document.querySelector("#newGame");
+const timerElement = document.getElementById("time");
 
 let timeLeft=60;
 
@@ -36,40 +36,33 @@ function getQuestions() {
     myRequest.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let questions = JSON.parse(this.responseText);
-            //Number Of Question Each New Game
             let qCount = 10;
             questionNum(qCount);
-            //Random Question Each New Game
-            questions = questions.sort(() => Math.random() - Math.random()).slice(0, 10);
+    questions = questions.sort(() => Math.random() - Math.random()).slice(0, 10);
 
-            //Add Questions Data
             addQuestionData(questions[currentIndex], qCount);
 
             flagLis.forEach(li => {
-                li.addEventListener('click', () => {
+                li.addEventListener("click", () => {
                     let rightAnswer = questions[currentIndex].right_answer;
-                    li.classList.add('active');
-                    //Increase Index 
+                    li.classList.add("active");
+
                     currentIndex++;
 
-                    //Check The Answer after 500ms
+
                     setTimeout(() => {
                         checkAnswer(rightAnswer, qCount);
                     }, 500);
 
                     setTimeout(() => {
-                        //Remove Previous Image Source
-                        flagImg.src = '';
-                        //Remove All Classes (active,success,wrong)
-                        li.classList.remove('active');
-                        li.classList.remove('success');
-                        li.classList.remove('wrong');
+                        flagImg.src = "";
+                        li.classList.remove("active");
+                        li.classList.remove("success");
+                        li.classList.remove("wrong");
 
-                        //Add Questions Data To Show The Next Question
                         addQuestionData(questions[currentIndex], qCount);
                     }, 1000);
 
-                    //Show Results
                     setTimeout(() => {
                         showResults(qCount);
                     }, 1002);
@@ -105,37 +98,34 @@ function addQuestionData(obj, count) {
 function checkAnswer(rAnswer, count) {
     let choosenAnswer;
     for (let i = 0; i < flagLis.length; i++) {
-        if (flagLis[i].classList.contains('active')) {
+        if (flagLis[i].classList.contains("active")) {
             choosenAnswer = flagLis[i].dataset.answer;
             if (rAnswer === choosenAnswer) {
-                flagLis[i].classList.add('success');
+                flagLis[i].classList.add("success");
                 rightAnswer++;
                 score.innerHTML = rightAnswer;
             } else {
-                flagLis[i].classList.add('wrong');
+                flagLis[i].classList.add("wrong");
             }
         }
     }
 }
 
-//Function To Show result correct and wrong answer
 function showResults(count) {
     if (currentIndex === count) {
-        flagOptions.innerHTML = '';
-        flagImgDiv.innerHTML = '';
-        scoreDiv.style.display = 'block';
+        flagOptions.innerHTML = "";
+        flagImgDiv.innerHTML = "";
+        scoreDiv.style.display = "block";
         correctAns.innerHTML = rightAnswer;
         incorrectAns.innerHTML = count - rightAnswer;
     }
 }
 
-//To Generate A New Game
-btnNewGame.addEventListener('click', () => {
+btnNewGame.addEventListener("click", () => {
     window.location.reload();
 });
 
-//If Click For A Challenge Clicked
-btnChallenge.addEventListener('click'), () => {
+btnChallenge.addEventListener("click"), () => {
     startTimer(15); //calling startTimer function
     startTimerLine(0); //calling startTimerLine function
 }
