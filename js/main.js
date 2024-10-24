f95109439870fdbe70784f561cf90b8c791ab595
Georgina Kidger <georgina.kidger@hotmail.com>
2024-10-24 23:16:09 +0000

    added buttons to navigate difficulty as well as a timer

--- js/main.js
+++ js/main.js
@@ -103,3 +103,45 @@ btnNewGame.addEventListener("click", () => {
     window.location.reload();
 });
 
+function StartMyTimer() {
+    // Set the date we're counting down to 60 seconds
+    var countDownDate = new Date().getTime()+60000;
+    
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+    
+      // Get today's date and time
+      var now = new Date().getTime();
+        
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+        
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+      // Output the result in an element with id="TimerDisplay"
+      document.getElementById("TimerDisplay").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+        
+      // If the count down is over, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("TimerDisplay").innerHTML = "Time Out";
+        let element = document.getElementById("TimeoutTryAgain");
+        let hidden = element.getAttribute("hidden");
+        element.removeAttribute("hidden");
+      }
+    }, 1000);
+    }
+   
+  
+    function TryAgain() {
+        let element = document.getElementById("TimeoutTryAgain");
+        let hidden = element.getAttribute("hidden");
+        element.setAttribute("hidden", "hidden");
+        StartMyTimer();
+    }
+
